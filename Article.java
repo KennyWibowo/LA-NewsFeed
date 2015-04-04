ef0d9224f8ddad9b22a11cf94c3904c3cb5f7a54
Joseph Thomas <joseph.thomas.c@gmail.com>
2015-04-04 01:59:46 -0700

    Finished Article

---
+++ Article.java
@@ -0,0 +1,44 @@
+public class Article{
+   public String title, description, link;
+   public int priority;
+
+   public Article( String t, String d, String l, int p)
+   {
+      title = t;
+      description = d;
+      link = l;
+      priority = p;
+   }
+
+   public Article( String t, String d, String l)
+   {
+      title = t;
+      description = d;
+      link = l;
+   }
+
+   public String getTitle()
+   {
+      return title;
+   }
+
+   public String getDescription()
+   {
+      return description;
+   }
+
+   public String getLink()
+   {
+      return link;
+   }
+
+   public int getPriority()
+   {
+      return priority;
+   }
+
+   public int setPriority(int p)
+   {
+      priority = p;
+   }
+}
