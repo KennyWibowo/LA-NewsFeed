368effe05c56e9c78826d7f2562954d60b13618b
Kenny Wibowo <kewibowo@ucsd.edu>
2015-04-04 16:52:56 -0700

    Started initial GUI frontpage landing overlay
    With the slight mentoring and distant guidance from Varun

--- gen/com/app/la_newsfeed/R.java
+++ gen/com/app/la_newsfeed/R.java
@@ -34,20 +34,18 @@ public final class R {
         public static final int la_news=0x7f020003;
     }
     public static final class id {
-        public static final int action_example=0x7f080007;
-        public static final int action_settings=0x7f080006;
-        public static final int button1=0x7f080004;
+        public static final int action_example=0x7f080006;
+        public static final int action_settings=0x7f080005;
         public static final int container=0x7f080001;
         public static final int drawer_layout=0x7f080000;
-        public static final int navigation_drawer=0x7f080002;
-        public static final int section_label=0x7f080005;
-        public static final int textView1=0x7f080003;
+        public static final int layout=0x7f080002;
+        public static final int navigation_drawer=0x7f080003;
+        public static final int section_label=0x7f080004;
     }
     public static final class layout {
         public static final int activity_news_feed=0x7f030000;
-        public static final int activity_news_feed2=0x7f030001;
-        public static final int fragment_navigation_drawer=0x7f030002;
-        public static final int fragment_news_feed=0x7f030003;
+        public static final int fragment_navigation_drawer=0x7f030001;
+        public static final int fragment_news_feed=0x7f030002;
     }
     public static final class menu {
         public static final int global=0x7f070000;
@@ -59,6 +57,9 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int navigation_drawer_close=0x7f050005;
         public static final int navigation_drawer_open=0x7f050004;
+        public static final int text1=0x7f050008;
+        public static final int text2=0x7f050009;
+        public static final int text3=0x7f05000a;
         public static final int title_section1=0x7f050001;
         public static final int title_section2=0x7f050002;
         public static final int title_section3=0x7f050003;
--- src/com/main/Main.java
+++
@@ -1,9 +0,0 @@
-package com.main;
-
-public class Main {
-
-	public static void main(String[] args){
-		System.out.println("Derp.");
-	}
-
-}
