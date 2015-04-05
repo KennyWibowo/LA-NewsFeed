d87874b419d25bf75c8e0d2f618e4a830934cb90
Kenny Wibowo <kewibowo@ucsd.edu>
2015-04-04 23:29:51 -0700

    Cleanup

--- src/com/util/Links.java
+++ src/com/util/Links.java
@@ -2,28 +2,24 @@ package com.util;
 
 import java.util.ArrayList;
 
-public class Links{
-   public static ArrayList<typeLink> links;
+public class Links {
+	public static ArrayList<typeLink> links;
 
-   public Links()
-   {
-      if(links == null)
-      {
-         links = new ArrayList<typeLink>();
-      }
-   }
-   public void addLink(String l, String name)
-   {
-      if(!links.contains(new typeLink(l,name)))
-      {
-         links.add(new typeLink(l,name));
-      }
-   }
-   public  void removeLink(String l, String name)
-   {
-      if(links.contains(new typeLink(l,name)))
-      {
-         links.remove(new typeLink(l,name));
-      }
-   }
+	public Links() {
+		if (links == null) {
+			links = new ArrayList<typeLink>();
+		}
+	}
+
+	public void addLink(String l, String name) {
+		if (!links.contains(new typeLink(l, name))) {
+			links.add(new typeLink(l, name));
+		}
+	}
+
+	public void removeLink(String l, String name) {
+		if (links.contains(new typeLink(l, name))) {
+			links.remove(new typeLink(l, name));
+		}
+	}
 }
--- src/com/util/typeLink.java
+++ src/com/util/typeLink.java
@@ -1,24 +1,21 @@
 package com.util;
 
-public class typeLink{
-   public String url;
-   public String sourceName;
+public class typeLink {
+	public String url;
+	public String sourceName;
 
-   public typeLink(String u, String n)
-   {
-      url = u;
-      sourceName = n;
-   }
+	public typeLink(String u, String n) {
+		url = u;
+		sourceName = n;
+	}
 
-   public boolean equals(Object o)
-   {
-      if(o instanceof typeLink)
-      {
-         if(((typeLink)o).url == url && ((typeLink)o).sourceName == sourceName)
-         {
-            return true;
-         }
-      }
-      return false;
-   }
+	public boolean equals(Object o) {
+		if (o instanceof typeLink) {
+			if (((typeLink) o).url == url
+					&& ((typeLink) o).sourceName == sourceName) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
