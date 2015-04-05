e735113b61987765374990d0696ecdb8d21c658e
Kenny Wibowo <kewibowo@ucsd.edu>
2015-04-04 17:33:02 -0700

    added onCreate, cleaned up code

--- gen/com/app/la_newsfeed/R.java
+++ gen/com/app/la_newsfeed/R.java
@@ -57,9 +57,6 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int navigation_drawer_close=0x7f050005;
         public static final int navigation_drawer_open=0x7f050004;
-        public static final int text1=0x7f050008;
-        public static final int text2=0x7f050009;
-        public static final int text3=0x7f05000a;
         public static final int title_section1=0x7f050001;
         public static final int title_section2=0x7f050002;
         public static final int title_section3=0x7f050003;
